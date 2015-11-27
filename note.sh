6aab8535e1d21222a02df01adb66686a7bcf2f8c
Ryan Donnelly <herbivore.linux@gmail.com>
2015-11-27 03:24:23 -0500

    date stamp works

--- note.sh
+++ note.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 _file="~/notes/`date +%Y-%m-%d`"
-_date=`date +%A_%b_%d`
+_date=`date +%A_%b_%d,_%Y`
 
-if [[ ! -e $_file ]]; then 
+if [[ ! -e "$_file" ]]; then 
 	touch  ~/notes/`date +%Y-%m-%d` 
 	echo "Notes:" >> ~/notes/`date +%Y-%m-%d` 
-	cat "$_date" >> ~/notes/`date +%Y-%m-%d` 
+	echo "$_date" >> ~/notes/`date +%Y-%m-%d` 
 fi
 
 vim ~/notes/`date +%Y-%m-%d`
