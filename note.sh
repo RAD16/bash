cb9fa5cf14d6cc6ab1edd6f81d4514ca225e0b90
Ryan Donnelly <herbivore.linux@gmail.com>
2015-11-28 10:30:22 -0500

    "new variables"

--- note.sh
+++ note.sh
@@ -2,15 +2,19 @@
 
 _file="$HOME/notes/`date +%Y-%m-%d`"
 _fileprefix="$HOME/notes/"
-_date=`date +%A,\ %b\ %d,\ %Y`
+_datelong=`date +%A,\ %b\ %d,\ %Y`
+_dateshort=`date +%Y-%m-%d`
 
 if [ $# -eq 1 ]; then
-	vim "$_fileprefix""$1"
+	_argfile="$#"
+	touch "$_argfile"
+	echo "$_dateshort" >> "$_argfile"
+	vim "$_argfile"
 fi
 
 if [ ! -e "$_file" ]; then 
 	touch "$_file"
-	echo "$_date" >> "$_file"
+	echo "$_datelong" >> "$_file"
 	echo "======================" >> "$_file"
 	vim ~/notes/`date +%Y-%m-%d`
 fi
