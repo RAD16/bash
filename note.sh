28869030535830f95e319b90e7b339f7c48e1a83
Ryan Donnelly <herbivore.linux@gmail.com>
2015-11-28 10:09:00 -0500

    "date stamp has escaped spaces"

--- note.sh
+++ note.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 _file="$HOME/notes/`date +%Y-%m-%d`"
-_date=`date +%A_%b_%d,_%Y`
+_date=`date +%A,\ %b\ %d,\ %Y`
 
 if [ ! -e "$_file" ]; then 
 	touch "$_file"
-	echo "Notes:" >> "$_file"
 	echo "$_date" >> "$_file"
+	echo "======================" >> "$_file"
 	vim ~/notes/`date +%Y-%m-%d`
 fi
 
